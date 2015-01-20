a52833bca50526eb08f6366100632fdf5e08beca
Vikash <vikash.kmc@gmail.com>
2015-01-20 22:25:53 +0530

    Add example of echo command.

---
+++ src/commands/echo.sh
@@ -0,0 +1,18 @@
+# Description: echo - Display a line of text
+
+# Notes
+# None
+
+# Common Examples
+echo Hello World
+echo "Hello World"
+echo 'Hello World'
+
+# Examples with details
+echo <string>               # Displays text. <string> can be in single quotes, double quotes or without quotes.
+
+# Cool Tricks
+# None
+
+# TODO
+# None
