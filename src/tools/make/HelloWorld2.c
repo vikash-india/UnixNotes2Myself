613f2fa4bf9d9b637849401288b62369f9c390fb
Vikash <vikash.kmc@gmail.com>
2019-02-08 12:19:46 +0530

    Add make utility.

---
+++ src/tools/make/HelloWorld2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : CProject.c
+ Author      : Vikash
+ Version     :
+ Copyright   : (c) Vikash
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
